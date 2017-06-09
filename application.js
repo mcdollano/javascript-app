e3c7cdb8eb7c04b1facfe622fe1ed0311bdb282e
mcdollano <mcdollano@gmail.com>
2017-06-09 01:07:28 -0700

    javascript app

---
+++ application.js
@@ -0,0 +1,31 @@
+// <script type="text/javascript">
+// 	var questions = [
+// 		['Whos that pokemon?', 'Pikachu'],
+// 		['Whos that pokemon?', 'Meowth'],
+// 		['Whos that pokemon?', 'Charmander'],
+// 		['Whos that pokemon?', 'Squirttle'],
+// 		['Whos that pokemon?', 'Bulbasaur'],
+// 		];
+
+// 	var counter = 0;
+// 	var score = 0;
+
+// 	document.getElementById('btn').onclick=function(){
+// 		document.getElementById('question').innerHTML = questions[counter][0];
+// 		var input = document.getElementById('answer').value;
+
+// 		if (input == questions[counter][1]) {
+// 			document.getElementById('test').innerHTML = "Correct!";
+// 			score++;
+// 		}else{
+// 			document.getElementById('test').innerHTML = "    Wrong!";
+// 		}
+// 		document.getElementById('answer').value = '';
+// 		counter++;
+
+// 		if(counter >= questions.length){
+// 			alert(score + ' out of 5');
+// 			}
+// 	} 
+	
+// </script>
